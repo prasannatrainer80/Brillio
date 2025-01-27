ec38a07ebd63776024e7d79989cdaa6837d8e005
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-01-27 10:43:07 +0530

    code

--- Day24/UserDataDetails.java
+++ Day24/UserDataDetails.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.example.demo.model.UserData;
 
-
 public class UserDataDetails implements UserDetails {
 
 	 private String name;
@@ -26,23 +25,38 @@ public class UserDataDetails implements UserDetails {
 	                .collect(Collectors.toList());
 	    }
 
-	
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	    @Override
+	    public Collection<? extends GrantedAuthority> getAuthorities() {
+	        return authorities;
+	    }
 
-	@Override
-	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	    @Override
+	    public String getPassword() {
+	        return password;
+	    }
 
-	@Override
-	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	    @Override
+	    public String getUsername() {
+	        return name;
+	    }
 
+	    @Override
+	    public boolean isAccountNonExpired() {
+	        return true;
+	    }
+
+	    @Override
+	    public boolean isAccountNonLocked() {
+	        return true;
+	    }
+
+	    @Override
+	    public boolean isCredentialsNonExpired() {
+	        return true;
+	    }
+
+	    @Override
+	    public boolean isEnabled() {
+	        return true;
+	    }
 }
